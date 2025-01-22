d42b287eb62ff5687f2ea12c5dda25446a598ef2
Shlok Maktarpara <shlokmaktarpara023@gmail.com>
2025-01-23 02:31:37 +0530

    sign-up page completed

--- KanbanBoardApp/users/urls.py
+++ KanbanBoardApp/users/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("logout", views.logout_view, name="logout")
+    path("", views.home, name="home"),
+    path("logout", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    # path("login_user", views.login_user, name="login_user"),
+    # path("logout_user", views.logout_user, name="logout_user"),
 ]
