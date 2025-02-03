92f0c51de0b530a4d8fca17584d8536946f5b2be
RajGajjar04 <irajgajjar.2004@gmail.com>
2025-02-04 00:21:07 +0530

    Additional Commit

--- KanbanBoardApp/KanbanBoardApp/urls.py
+++ KanbanBoardApp/KanbanBoardApp/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
+# Comment --> Additional push
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
