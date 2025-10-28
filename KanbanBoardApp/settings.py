0a17be02d01fb146474490e469cc158985cab831
RajGajjar-01 <irajgajjar.2004@gmail.com>
2025-10-28 22:12:32 +0530

    refactor: deleting unnecessary files and adding tailwind support

--- KanbanBoardApp/settings.py
+++ KanbanBoardApp/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "board",
     "users",
+    'KanbanBoardApp',
     # Related to Google Authentication. For more info go to Notes.md or allauth documentation
     "allauth",
     "allauth.account",
