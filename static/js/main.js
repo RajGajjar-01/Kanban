ce09fdffedc1977861ff14e0d31a97fff6d3e270
RajGajjar-01 <irajgajjar.2004@gmail.com>
2025-11-01 01:42:42 +0530

    fix: remove unnecessary code and let templates use htmx minified rather than cdn

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,4 @@
-import { initNavbar } from './components/navbar.js';
-import { initTheme } from './utils/theme.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  initTheme();
-  initNavbar();  
+
 });
--- static/js/utils.js
+++ static/js/utils.js
@@ -1,5 +1,3 @@
-import { icon_sun, icon_moon } from './constants.js';
-
 export function openModal(modalId) {
     document.getElementById(modalId).classList.remove("hidden");
 }
@@ -21,33 +19,5 @@ export function getCookie(name) {
         }
     }
     return cookieValue;
-}
-
-export function initThemeToggle() {
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.innerHTML = localStorage.getItem('theme') === 'dark' ? icon_moon : icon_sun;
-    
-    if (themeToggle) {
-        themeToggle.addEventListener('click', () => {
-            console.log ('Theme toggle clicked');
-            document.documentElement.classList.toggle('dark');
-            // Change the icon
-            if (document.documentElement.classList.contains('dark')) {
-                themeToggle.innerHTML =  icon_moon;
-            } else {
-                themeToggle.innerHTML = icon_sun;
-            }
-            localStorage.setItem('theme', 
-                document.documentElement.classList.contains('dark') ? 'dark' : 'light'
-            );
-        });
-        
-
-        if (localStorage.getItem('theme') === 'dark' || 
-            (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark');
-
-        }
-    }
 }
 
